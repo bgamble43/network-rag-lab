86773cb897e8caf38ce4622466325cc8cd8b9a9c
bgamble43 <62437531+bgamble43@users.noreply.github.com>
2025-06-17 20:35:29 -0400

    Update rag_engine.py
    Updated imports

--- rag_engine.py
+++ rag_engine.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
